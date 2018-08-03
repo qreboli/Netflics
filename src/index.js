1460655e4397c279c9d7a1a1837d26de73aff1c5
qreboli <reboli.quentin@gmail.com>
2018-08-03 11:24:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react'
+
+const API_KEY = 'c67d38ce50b627d15f22af99f4e92da1';
+
+const App = function () {
+  return <div>Coucou les amis</div>
+}
+ReactDom.render(<App/>, document.querySelector('.container'));
