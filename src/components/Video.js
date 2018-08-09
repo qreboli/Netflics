68d9d8fe33ef77a27e3f4da6a98d34496cea250f
qreboli <reboli.quentin@gmail.com>
2018-08-09 11:13:25 +0200

    finish search bar & recommendation

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,10 +3,9 @@ import React from 'react'
 const  BASE_URL = 'https://www.youtube.com/embed/';
 
 const Video = ({videoId}) => {
-  console.log('toto', `${BASE_URL}${videoId}`);
   return(
-    <div>
-      <iframe src={`${BASE_URL}${videoId}`}/>
+    <div className='embed-responsive embed-responsive-16by9'>
+      <iframe className='embed-responsive-item' src={`${BASE_URL}${videoId}`}/>
     </div>
   )
 }
--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -2,11 +2,23 @@ import React from 'react'
 
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
 
-const VideoListItem = ({movie}) => {
-  return <li>
-    <img height='100px' width='auto' src={`${IMAGE_BASE_URL}${movie.poster_path}`}/>
-    <p>{movie.title}</p>
+const VideoListItem = (props) => {
+  const { movie } = props;
+  return <li className='list-group-item' onClick={handleOnClick}>
+    <div className='media'>
+      <div className='media-left'>
+        <img className='media-object img-rounded' height='100px' width='auto'
+             src={`${IMAGE_BASE_URL}${movie.poster_path}`}/>
+      </div>
+      <div className='media-body'>
+        <h5 className='title_list_item'>{movie.title}</h5>
+      </div>
+    </div>
   </li>
+
+  function handleOnClick(){
+    props.callback(movie);
+  }
 }
 
 export default VideoListItem;
